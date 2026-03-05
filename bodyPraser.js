5fcc9ba16b3023335ef12b973a52eaf1b28798eb
Deepthi1024ai <morlaposisrideepthi@gmail.com>
2026-03-05 10:48:49 +0530

    Implement basic login server with Express

---
+++ bodyPraser.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Default route - Login form
+app.get("/", (req, res) => {
+  res.send(`
+    <h2>Login Page</h2>
+    <form method="POST" action="/login">
+      Username: <input type="text" name="username" /><br><br>
+      Password: <input type="password" name="password" /><br><br>
+      <button type="submit">Login</button>
+    </form>
+  `);
+});
+
+// Handle Login
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === "admin" && password === "1234") {
+    res.send("<h2>login succesful ! welcome admin</h2>");
+  } else {
+    res.send("<h2>invalid username or password</h2>");
+  }
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.log("Server running on http://localhost:3000");
+});
